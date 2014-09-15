3f60ab6e6f39eb5676ec4fc33c5c8a983cd2e70b
Aaron Hill <aahill50@gmail.com>
2014-09-14 22:53:15 -0400

    Converted @dictionary from Array to Set'

--- WordChainer.rb
+++ WordChainer.rb
@@ -1,8 +1,11 @@
+require 'set'
+
 class WordChainer
 	attr_reader :dictionary
 
 	def initialize(dictionary_file_name = "dictionary.txt")
 		@dictionary = File.open(dictionary_file_name).readlines.map(&:chomp)
+		@dictionary = Set.new(@dictionary)
 	end
 
 	def adjacent_words(word)
@@ -23,4 +26,4 @@ class WordChainer
 end
 
 # test_wc = WordChainer.new
-# p test_wc.adjacent_words("money")
+# p test_wc.adjacent_words("cant")
