1a40165d7a71513d5fb24889e6a8bf9b17958528
Aaron Hill <aahill50@gmail.com>
2014-09-14 22:50:49 -0400

    Working method for pulling all adjacent words

--- WordChainer.rb
+++ WordChainer.rb
@@ -1,3 +1,26 @@
 class WordChainer
+	attr_reader :dictionary
 
+	def initialize(dictionary_file_name = "dictionary.txt")
+		@dictionary = File.open(dictionary_file_name).readlines.map(&:chomp)
+	end
+
+	def adjacent_words(word)
+		adjacent_words = []
+		letters = word.split(//)
+		dict = self.dictionary.dup
+
+		letters.each_with_index do |letter, index|
+			('a'..'z').each do |char|
+				new_word = word.dup
+				new_word[index] = char
+				adjacent_words << new_word if dict.include?(new_word)
+			end
+		end
+
+		adjacent_words.reject {|adj| adj == word}
+	end
 end
+
+# test_wc = WordChainer.new
+# p test_wc.adjacent_words("money")
